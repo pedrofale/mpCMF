24859ad43247b245a290edb013b1647fb5d9aeb6
pedrofale <pedro.miguel.ferreira.pf@gmail.com>
2018-03-27 17:51:01 +0100

    bug fix

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ def generate_U(N, K, C=2, alpha=1., eps=5.):
 	clusters = np.zeros((N,))
 	for c in range(C):
 		clusters[int(c*N/C):int((c+1)*N/C)] = clusters[int(c*N/C):int((c+1)*N/C)] + c
-		U[int(c*K/C):int((c+1)*K/C), int(c*N/C):int((c+1)*N/C)] = sample_gamma(alpha + eps, 1., size=(int(K/C), int(K/C)))
+		U[int(c*K/C):int((c+1)*K/C), int(c*N/C):int((c+1)*N/C)] = sample_gamma(alpha + eps, 1., size=(int(K/C), int(N/C)))
 	return U, clusters
 
 def generate_data(N, P, K, U=None, C=2, alpha=1., eps=5., shape=2., rate=2., zero_prob=0.5, return_all=False):
