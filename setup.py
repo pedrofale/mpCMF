2f38b3ea67156eb9e16a1924ea3166c3a513a723
pedrofale <pedro.miguel.ferreira.pf@gmail.com>
2018-05-15 10:51:05 +0100

    add setup.py to install via pip

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SVIpCMF",
+    version="0.1",
+    author="Pedro Ferreira",
+    description="Probabilistic Count Matrix Factorization using stochastic variational inference.",
+    packages=find_packages(),
+)
