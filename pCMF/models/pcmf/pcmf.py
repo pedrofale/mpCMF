b2870e690bfe675871d0cef73b80675948bb157e
pedrofale <pedro.miguel.ferreira.pf@gmail.com>
2018-05-29 18:34:52 +0100

    PCMF class

--- pCMF/models/pcmf/pcmf.py
+++ pCMF/models/pcmf/pcmf.py
@@ -1,58 +1,110 @@
 import numpy as np
-from pCMF.models.pcmf import cavi, svi
+from pCMF.models.pcmf.inferences import cavi_new, svi_new
 from pCMF.misc import utils
 
+from sklearn.manifold import TSNE
+from sklearn.metrics import silhouette_score, accuracy_score
+
 class PCMF(object):
-	def __init__(self, train_data, n_components=10, sampling_rate=1, max_time=60, alpha=None, beta=None, pi_D=None, pi_S=None, verbose=True):
-		self.train_data = train_data
+	def __init__(self, X_train, c_train, D_train=None, X_test=None, c_test=None, n_components=10, empirical_bayes=True, 
+					minibatch_size=None, alpha=None, beta=None, pi_D=None, pi_S=None, zero_inflation=True, sparsity=True, name=None):
+		self.X_train = X_train
+		self.c_train = c_train
 
-		P = train_data.shape[1]
+		self.D_train = D_train
+		self.dropout_idx = None
+		if D_train is not None:
+			self.dropout_idx = np.where(self.D_train == 0)
 
-		self.inferences = ['gibbs', 'cavi', 'svi']
+		self.X_test = X_test
+		self.c_test = c_test
 
-		self.n_components = n_components
-		self.sampling_rate = sampling_rate
-		self.max_time = max_time
+		if X_test is not None:
+			assert X_train.shape[1] == X_test.shape[1]
 
-		self.verbose = verbose
+		self.P = X_train.shape[1]
+
+		self.minibatch_size = minibatch_size
+		self.empirical_bayes = empirical_bayes
+
+		self.K = n_components
 
 		self.alpha = alpha
 		self.beta = beta
 		self.pi_D = pi_D
+		self.pi_S = pi_S
 
 		if alpha is None:
-			self.alpha = np.ones((2, n_components))
-			self.alpha[0, :] = 3.
-			self.alpha[1, :] = 0.5
+			self.alpha = np.abs(np.ones((2, self.K)) + np.random.rand(2, self.K))
 		if beta is None:
-			self.beta = np.ones((2, P, n_components))
-		if pi_D is None:
-			self.pi_D = np.ones((P,)) * 0.5
+			self.beta = np.abs(np.ones((2, self.P, self.K)) + np.random.rand(2, self.P, self.K))
+		if pi_D is None and zero_inflation:
+			logit_pi_D = np.random.rand(self.P)
+			self.pi_D = np.exp(logit_pi_D) / (1. + np.exp(logit_pi_D))
+		if pi_S is None and sparsity:
+			logit_pi_S = np.random.rand(self.P)
+			self.pi_S = np.exp(logit_pi_S) / (1. + np.exp(logit_pi_S))
 
-		self.inf = None
-		self.ll = None
 		self.est_U = None
 		self.est_V = None
+		self.est_D = None
+		self.est_S = None
+		self.est_R = None
+		self.proj_2d = None
+		self.train_ll = None
+		self.test_ll = None
+		self.silhouette = None
+		self.dropid_acc = None
+		self.dropimp_err = None
 
-	def infer(self, test_data=None, algorithm='cavi', empirical_bayes=True, minibatch_size=1, n_iterations=100):
-		if algorithm in self.inferences:
-			if algorithm is 'cavi':
-				# Run CAVI and get estimates
-				self.inf = cavi.CoordinateAscentVI(self.train_data, self.alpha, self.beta, self.pi_D)
-				self.ll = self.inf.run_cavi(X_test=test_data, n_iterations=n_iterations, empirical_bayes=empirical_bayes, return_ll=True, 
-					sampling_rate=self.sampling_rate, max_time=self.max_time, verbose=self.verbose)
-				self.est_U = self.inf.a[0] / self.inf.a[1] # VI estimate is the mean of the variational approximation
-				self.est_V = self.inf.b[0] / self.inf.b[1] # VI estimate is the mean of the variational approximation
+		self.name = name
+		if self.name is None:
+			if minibatch_size is None:
+				if empirical_bayes:
+					self.name = "CAVI-EB"
+				else:
+					self.name = "CAVI"
+			else:
+				if empirical_bayes:
+					self.name = "SVI-EB-{}".format(minibatch_size)
+				else:
+					self.name = "SVI-{}".format(minibatch_size)
 
-			if algorithm is 'svi':
-				# Run SVI and get estimates
-				self.inf = svi.StochasticVI(self.train_data, self.alpha, self.beta, self.pi_D)
-				self.ll = self.inf.run_svi(X_test=test_data, n_iterations=n_iterations, empirical_bayes=empirical_bayes, minibatch_size=minibatch_size, return_ll=True, 
-					sampling_rate=self.sampling_rate, max_time=self.max_time, verbose=self.verbose)
-				self.est_U = self.inf.a[0] / self.inf.a[1] # VI estimate is the mean of the variational approximation
-				self.est_V = self.inf.b[0] / self.inf.b[1] # VI estimate is the mean of the variational approximation
+		self.inf = None
+		print("{}:".format(self.name))
+		if minibatch_size is None:
+			self.inf = cavi_new.CoordinateAscentVI(X_train, self.alpha, self.beta, pi_D=self.pi_D, pi_S=self.pi_S, empirical_bayes=empirical_bayes)
 		else:
-			print('Inference algorithm unrecognized.')
+			self.inf = svi_new.StochasticVI(X_train, self.alpha, self.beta, pi_D=self.pi_D, pi_S=self.pi_S, minibatch_size=minibatch_size, empirical_bayes=empirical_bayes)
+
+	def run(self, max_iter=1, sampling_rate=1, max_time=60, calc_ll=True, calc_silh=True, do_tsne=True, do_dll=True, do_holl=True, do_silh=True, do_imp=True, verbose=False):
+		if verbose:
+			print('Running {0}...'.format(self.name))
+		self.inf.run(n_iterations=max_iter, calc_ll=calc_ll, calc_silh=calc_silh, clusters=self.c_train, sampling_rate=sampling_rate, max_time=max_time)
+		
+		self.est_U = self.inf.a[0] / self.inf.a[1] # VI estimate is the mean of the variational approximation
+		self.est_V = self.inf.b[0] / self.inf.b[1]
+		self.est_D = self.inf.estimate_D(self.inf.p_D)
+		self.est_S = self.inf.estimate_S(self.inf.p_S)
+		self.est_R = np.dot(self.est_U, self.est_V.T)
+
+		if do_tsne:
+			self.proj_2d = TSNE(n_components=2).fit_transform(self.est_U)
+
+		if do_dll:
+			self.train_ll = utils.log_likelihood(self.X_train, self.est_U, self.est_V, self.inf.p_D, self.est_S, clip=self.inf.clip_ll)
+		
+		if do_holl:
+			if self.X_test is not None:
+				self.test_ll = self.inf.predictive_ll(self.X_test)
+
+		if do_silh:
+			self.silhouette = silhouette_score(self.est_U, self.c_train)
+
+		if do_imp:
+			if self.D_train is not None:
+				self.dropid_acc = accuracy_score(self.est_D.flatten(), self.D_train.flatten())
+				self.dropimp_err = utils.imputation_error(self.X_train, self.est_R, self.dropout_idx)
 
 	def sample_posterior(return_all=False):
 		U = utils.sample_gamma(self.inf.a[0], self.inf.a[1])
