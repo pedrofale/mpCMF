64d3b96ab711772068cf8ff920797eeb1dd9f1ab
pedrofale <pedro.miguel.ferreira.pf@gmail.com>
2018-10-05 00:25:17 +0100

    remove noise

--- pCMF/models/hpcmf/inferences/__init__.py
+++
@@ -1,3 +0,0 @@
-from pCMF.models.hpcmf.inferences import klqp
-from pCMF.models.hpcmf.inferences import cavi
-from pCMF.models.hpcmf.inferences import svi
--- pCMF/models/pcmf/inferences/__init__.py
+++
@@ -1,4 +0,0 @@
-from pCMF.models.pcmf.inferences import klqp
-from pCMF.models.pcmf.inferences import cavi
-from pCMF.models.pcmf.inferences import svi
-from pCMF.models.pcmf.inferences import gibbs
